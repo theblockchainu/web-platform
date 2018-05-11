162799149093d1e217878319d5d63f438ab427c2
donatedTunic1290 <connect@aakashbansal.com>
2018-05-11 12:49:33 +0530

    Bug fixes UI fixes

--- src/app/_services/wallet/wallet.service.ts
+++ src/app/_services/wallet/wallet.service.ts
@@ -6,36 +6,39 @@ import { RequestHeaderService } from '../requestHeader/request-header.service';
 
 @Injectable()
 export class WalletService {
-
-  private userId: string;
-  private options: any;
-  constructor(
-    private http: HttpClient,
-    private _cookieUtilsService: CookieUtilsService,
-    private requestHeaderService: RequestHeaderService
-  ) {
-    this.userId = _cookieUtilsService.getValue('userId');
-    this.options = requestHeaderService.getOptions();
-  }
-
-  /**
-   * getWallet
-   */
-  public getWallet() {
-    return this.http.get(environment.apiUrl + '/api/peers/' + this.userId + '/wallet', this.options);
-  }
-
-
-
-  /**
-   * createWallet
-   */
-  public createWallet() {
-    return this.http.post(environment.apiUrl + '/api/peers/' + this.userId + '/wallet', {
-      'gyan_balance': 0,
-      'karma_balance': 0
-    }, this.options);
-
-  }
-
+	
+	private userId: string;
+	private options: any;
+	constructor(
+		private http: HttpClient,
+		private _cookieUtilsService: CookieUtilsService,
+		private requestHeaderService: RequestHeaderService
+	) {
+		this.userId = _cookieUtilsService.getValue('userId');
+		this.options = requestHeaderService.getOptions();
+	}
+	
+	/**
+	 * getWallet
+	 */
+	public getWallet() {
+		return this.http.get(environment.apiUrl + '/api/peers/' + this.userId + '/wallet', this.options)
+			.map((response: any) => response, (err) => {
+				console.log(err);
+				});
+	}
+	
+	
+	
+	/**
+	 * createWallet
+	 */
+	public createWallet() {
+		return this.http.post(environment.apiUrl + '/api/peers/' + this.userId + '/wallet', {
+			'gyan_balance': 0,
+			'karma_balance': 0
+		}, this.options);
+		
+	}
+	
 }
--- src/app/console/console-account/console-account-wallet/console-account-wallet.component.ts
+++ src/app/console/console-account/console-account-wallet/console-account-wallet.component.ts
@@ -6,69 +6,78 @@ import { CookieUtilsService } from '../../../_services/cookieUtils/cookie-utils.
 import { ProfileService } from '../../../_services/profile/profile.service';
 
 @Component({
-  selector: 'app-console-account-wallet',
-  templateUrl: './console-account-wallet.component.html',
-  styleUrls: ['./console-account-wallet.component.scss']
+	selector: 'app-console-account-wallet',
+	templateUrl: './console-account-wallet.component.html',
+	styleUrls: ['./console-account-wallet.component.scss']
 })
 export class ConsoleAccountWalletComponent implements OnInit {
-
-  public wallet: any;
-  public showWalletId = false;
-  private userId: string;
-
-  constructor(
-    public consoleAccountComponent: ConsoleAccountComponent,
-    public activatedRoute: ActivatedRoute,
-    private _walletService: WalletService,
-    private _profileService: ProfileService,
-    private _cookieUtilsService: CookieUtilsService,
-  ) {
-    activatedRoute.pathFromRoot[4].url.subscribe((urlSegment) => {
-      console.log(urlSegment[0].path);
-      consoleAccountComponent.setActiveTab(urlSegment[0].path);
-    });
-    this.userId = _cookieUtilsService.getValue('userId');
-  }
-
-  ngOnInit() {
-    this.fetchWallet();
-    this.fetchTransactions();
-  }
-
-  private fetchWallet() {
-    this._walletService.getWallet().subscribe(res => {
-      if (res && res[0]) {
-        this.wallet = res[0];
-      }
-    });
-  }
-
-  private fetchTransactions() {
-    const query = {
-      'include': [
-        {
-          'token_transactions': [
-            'collections',
-            'communities',
-            'contents',
-            'peers'
-          ]
-        }
-      ]
-    };
-    this._profileService.getPeerData(this.userId, query).subscribe(res => {
-      console.log(res);
-    });
-  }
-
-  public createWallet() {
-    this._walletService.createWallet().subscribe(res => {
-      this.fetchWallet();
-    });
-  }
-
-  public toggleWalletId() {
-    this.showWalletId = !this.showWalletId;
-  }
-
+	
+	public wallet: any;
+	public showWalletId = false;
+	private userId: string;
+	public peer: any;
+	public loadingWallet = true;
+	public loadingPeer = true;
+	
+	constructor(
+		public consoleAccountComponent: ConsoleAccountComponent,
+		public activatedRoute: ActivatedRoute,
+		private _walletService: WalletService,
+		private _profileService: ProfileService,
+		private _cookieUtilsService: CookieUtilsService,
+	) {
+		activatedRoute.pathFromRoot[4].url.subscribe((urlSegment) => {
+			console.log(urlSegment[0].path);
+			consoleAccountComponent.setActiveTab(urlSegment[0].path);
+		});
+		this.userId = _cookieUtilsService.getValue('userId');
+	}
+	
+	ngOnInit() {
+		this.fetchWallet();
+		this.fetchTransactions();
+	}
+	
+	private fetchWallet() {
+		this.loadingWallet = true;
+		this._walletService.getWallet().subscribe(res => {
+			if (res && res.length > 0) {
+				this.wallet = res[0];
+				this.loadingWallet = false;
+			} else {
+				this.wallet = {};
+				this.loadingWallet = false;
+			}
+		}, err => {
+			this.wallet = {};
+			this.loadingWallet = false;
+		});
+	}
+	
+	private fetchTransactions() {
+		this.loadingPeer = true;
+		const query = {
+			'include': [
+				{
+					'token_transactions': [
+						'collections',
+						'communities',
+						'contents',
+						'peers'
+					]
+				}
+			]
+		};
+		this._profileService.getPeerData(this.userId, query).subscribe(res => {
+			this.peer = res;
+			this.loadingPeer = false;
+		}, err => {
+			this.peer = {};
+		});
+	}
+	
+	public toggleWalletId() {
+		this.showWalletId = !this.showWalletId;
+	}
+	
 }
