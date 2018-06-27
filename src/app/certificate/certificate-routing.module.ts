8deb6113b1a878acf50e78edc43e0f077cd9eb53
abhijeet <abhijeet1abhijeet@gmail.com>
2018-06-28 03:20:19 +0530

    certificate fixes

---
+++ src/app/certificate/certificate-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CertificatePageComponent } from './certificate-page/certificate-page.component';
+import { AuthGuardService } from '../_services/auth-guard/auth-guard.service';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: ':certificateId',
+        component: CertificatePageComponent,
+        canActivateChild: [AuthGuardService]
+      }
+
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CertificateRoutingModule { }
