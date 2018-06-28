ef1e61acc418f31dae082627a49aed51160ebf80
abhijeet <abhijeet1abhijeet@gmail.com>
2018-06-28 07:00:03 +0530

    fixes

--- src/app/_services/dialogs/certificate-verification/certificate-verification.component.ts
+++ src/app/_services/dialogs/certificate-verification/certificate-verification.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
 import { CertificateService } from '../../certificate/certificate.service';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-certificate-verification',
   templateUrl: './certificate-verification.component.html',
   styleUrls: ['./certificate-verification.component.scss']
 })
 export class CertificateVerificationComponent implements OnInit {
+  public signature: any;
+  public verified: boolean;
+  public loading: boolean;
 
   constructor(
     private certificateService: CertificateService,
@@ -15,9 +19,27 @@ export class CertificateVerificationComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.certificateService.getBlockchainHash(this.data.collection.id, this.data.recipient.ethAddress).subscribe(res => {
-      console.log(res);
-    });
+    this.loading = true;
+    this.verified = false;
+    this.hashData();
   }
 
+  hashData() {
+    this.signature = this.data.signature;
+    const certificateData = _.cloneDeep(this.data);
+    delete certificateData.signature;
+    const sha = this.certificateService.getSHA(JSON.stringify(certificateData)).slice(0, 32);
+    console.log(sha);
+    this.certificateService.getBlockchainHash(this.data.collection.id, this.data.recipient.ethAddress)
+      .subscribe((res: any) => {
+        console.log(res);
+        if (res === sha) {
+          this.verified = true;
+          console.log('Its a match');
+          this.loading = false;
+        } else {
+          console.log('err');
+        }
+      });
+  }
 }
