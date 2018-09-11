6e79e06952801e18bd19457ddfb58079947c3e8a
donatedTunic1290 <connect@aakashbansal.com>
2018-09-11 19:24:38 +0530

    updates to landing page bug fixes

--- projects/BlockchainU/src/app/class/class-page/class-page.component.ts
+++ projects/BlockchainU/src/app/class/class-page/class-page.component.ts
@@ -614,31 +614,36 @@ export class ClassPageComponent implements OnInit, OnDestroy {
 				},
 					err => console.log('error'),
 					() => {
-						this.initializeUserType();
-						this.calculateTotalHours();
-						this.fixTopics();
-						this.getReviews();
-						this.getRecommendations();
-						this.getParticipants();
-						this.getDiscussions();
-						this.getBookmarks();
-						this.setUpCarousel();
-						this.getCertificatetemplate();
-						this.sortAssessmentRules();
-
-						if (this.toOpenDialogName !== undefined && this.toOpenDialogName !== 'paymentSuccess') {
-							this.itenaryArray.forEach(itinerary => {
-								itinerary.contents.forEach(content => {
-									if (content.id === this.toOpenDialogName) {
-										this.openDialog(content, itinerary.startDate, itinerary.endDate);
-									}
+						if (this.class && this.class.owners && this.class.owners.length > 0) {
+							this.initializeUserType();
+							this.calculateTotalHours();
+							this.fixTopics();
+							this.getReviews();
+							this.getRecommendations();
+							this.getParticipants();
+							this.getDiscussions();
+							this.getBookmarks();
+							this.setUpCarousel();
+							this.getCertificatetemplate();
+							this.sortAssessmentRules();
+							
+							if (this.toOpenDialogName !== undefined && this.toOpenDialogName !== 'paymentSuccess') {
+								this.itenaryArray.forEach(itinerary => {
+									itinerary.contents.forEach(content => {
+										if (content.id === this.toOpenDialogName) {
+											this.openDialog(content, itinerary.startDate, itinerary.endDate);
+										}
+									});
 								});
-							});
-						} else if (this.toOpenDialogName !== undefined && this.toOpenDialogName === 'paymentSuccess') {
-							const snackBarRef = this.snackBar.open('Your payment was successful. Happy learning!', 'Okay', { duration: 5000 });
-							snackBarRef.onAction().subscribe();
+							} else if (this.toOpenDialogName !== undefined && this.toOpenDialogName === 'paymentSuccess') {
+								const snackBarRef = this.snackBar.open('Your payment was successful. Happy learning!', 'Okay', { duration: 5000 });
+								snackBarRef.onAction().subscribe();
+							}
+							this.recordStartView();
+						} else {
+							this.snackBar.open('This online class has either been deleted or flagged.', 'OK', {duration: 5000});
+							this.router.navigate(['home', 'classes']);
 						}
-						this.recordStartView();
 					});
 		} else {
 			console.log('NO COLLECTION');
